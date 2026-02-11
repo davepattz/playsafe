cee1c1c8682f4bc8bbc9f637e8712422707bbab4
David Pattinson <david-pattinson@live.com>
2026-02-11 19:30:15 +1000

    Made footer black and white text.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="mt-10 border-t bg-white">
-      <div className="max-w-7xl mx-auto px-4 py-6 text-sm text-gray-500">
+    <footer className="mt-10 border-t bg-black">
+      <div className="max-w-7xl mx-auto px-4 py-6 text-sm text-white-500">
         © {new Date().getFullYear()} PlaySafe.games
       </div>
     </footer>
