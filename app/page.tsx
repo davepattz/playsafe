ba0c36e191d4fb029338d04ec31919e66b21ca57
David Pattinson <david-pattinson@live.com>
2026-02-14 20:02:26 +1000

    Fixed white bg in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Footer from "@/components/Footer"
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gray-50">
+    <main className="min-h-screen bg-white">
       <Header />
 
       <section className="max-w-7xl mx-auto px-4">
