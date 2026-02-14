2279115823ad50173d2dc516aeafb3ef701b48c6
David Pattinson <david-pattinson@live.com>
2026-02-14 19:28:46 +1000

    Styled basic filter section and stacks on mobile

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -1,9 +1,31 @@
 export default function Filters() {
   return (
-    <div className="mt-4 flex flex-wrap gap-4">
-      <div className="bg-gray-100 p-3 rounded">Age</div>
-      <div className="bg-gray-100 p-3 rounded">Genre</div>
-      <div className="bg-gray-100 p-3 rounded">Play Style</div>
+    <div className="mt-8 w-full flex flex-col md:flex-row">
+      
+      {/* Age */}
+      <div className="w-full md:w-1/3 flex justify-center items-center gap-2 py-6 cursor-pointer">
+        <span className="text-black text-[30px] font-bold font-['Lato']">
+          Age
+        </span>
+        <span className="text-black text-[12px]">▼</span>
+      </div>
+
+      {/* Genre */}
+      <div className="w-full md:w-1/3 flex justify-center items-center gap-2 py-6 cursor-pointer">
+        <span className="text-black text-[30px] font-bold font-['Lato']">
+          Genre
+        </span>
+        <span className="text-black text-[12px]">▼</span>
+      </div>
+
+      {/* Play Style */}
+      <div className="w-full md:w-1/3 flex justify-center items-center gap-2 py-6 cursor-pointer">
+        <span className="text-black text-[30px] font-bold font-['Lato']">
+          Play Style
+        </span>
+        <span className="text-black text-[12px]">▼</span>
+      </div>
+
     </div>
   )
 }
