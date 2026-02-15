2f54e3fdd047ed6baaf6ebc365fd9a485ce01388
David Pattinson <david-pattinson@live.com>
2026-02-15 19:10:16 +1000

    Styled sidebar ad placeholder

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,9 @@
 export default function Sidebar() {
   return (
-    <aside className="bg-white p-4 rounded shadow hidden lg:block">
-      Ad / Info Sidebar
-    </aside>
+    <div className="flex justify-center">
+        <div className="bg-gray-200 w-[300px] h-[600px] flex items-center justify-center text-gray-500">
+          300 × 600
+        </div>
+      </div>
   )
 }
