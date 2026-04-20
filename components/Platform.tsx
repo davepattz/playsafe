79a832a990496f4953e988d6cdf1b55c7b86b9c2
David Pattinson <david-pattinson@live.com>
2026-04-20 11:46:14 +1000

    Updated platform with steam logo

--- components/Platform.tsx
+++ components/Platform.tsx
@@ -2,7 +2,7 @@
 
 export default function Platform() {
   return (
-    <div className="flex flex-col items-center justify-center gap-2 w-full sm:w-auto sm:flex-row sm:items-center mx-auto py-6">
+    <div className="flex flex-col items-center justify-center gap-2 w-full sm:w-auto sm:flex-row sm:items-center mx-auto py-8">
       <span className="text-[18px] font-normal font-['Lato']">Platforms:</span>
 
       <div className="flex items-center gap-2 mt-1 sm:mt-0">
@@ -14,8 +14,8 @@ export default function Platform() {
           <img src="/apple_logo.svg" alt="macOS" className="w-[22px] h-auto" />
         </button>
 
-        <button type="button" aria-label="Filter Linux" className="w-8 h-8 flex items-center justify-center rounded-sm" onClick={() => {}}>
-          <img src="/linux_logo.svg" alt="Linux" className="w-[22px] h-auto" />
+        <button type="button" aria-label="Filter Steam" className="w-8 h-8 flex items-center justify-center rounded-sm" onClick={() => {}}>
+          <img src="/steam_logo.svg" alt="Steam" className="w-[22px] h-auto" />
         </button>
       </div>
     </div>
