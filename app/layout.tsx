44a58f6a0630c89e00e13918e866fce9ad9e7c36
David Pattinson <david-pattinson@live.com>
2026-04-29 19:05:09 +1000

    added google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Lato } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const lato = Lato({
@@ -30,6 +31,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-QXFD5D8991"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-QXFD5D8991');
+        `}
+      </Script>
       <body
         className={`${lato.variable} ${geistSans.variable} ${geistMono.variable} antialiased bg-white min-h-screen`}
       >
